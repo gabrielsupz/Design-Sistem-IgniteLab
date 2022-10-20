90c10f570dc7fc55bfb6943550658bc50921a4ff
Gabriel Suptitz <gabrielsuptitz@gmail.com>
2022-10-20 17:32:08 -0300

    Terminando

--- src/pages/SignIn.stories.tsx
+++ src/pages/SignIn.stories.tsx
@@ -8,22 +8,4 @@ export default {
   args: {},
   argTypes: {}
 } as Meta
-// export const Normal: StoryObj = {}
-// export const Filled: StoryObj = {
-//   play: async ({ canvasElement }) => {
-//     const canvas = within(canvasElement)
-
-//     userEvent.type(
-//       canvas.getByPlaceholderText('Digite seu e-mail'),
-//       'gabrielsuptitz@gmail.com'
-//     )
-
-//     userEvent.type(canvas.getByPlaceholderText('Digite sua senha'), '12312343')
-
-//     userEvent.click(canvas.getByRole('button'))
-
-//     await waitFor(() => {
-//       return expect(canvas.getByText('Login realizado!')).toBeInTheDocument()
-//     })
-//   }
-// }
+export const Normal: StoryObj = {}
